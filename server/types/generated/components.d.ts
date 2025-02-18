ac729008cbb0cc4fe038929a8441e4ae52c7ffe3
briancaldera07 <briancaldera07@gmail.com>
2025-02-18 02:23:05 +0000

    💎 checkpoint

--- server/types/generated/components.d.ts
+++ server/types/generated/components.d.ts
@@ -1,3 +1,143 @@
-/*
- * The app doesn't have any components yet.
- */
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface BlocksFeatureArticle extends Struct.ComponentSchema {
+  collectionName: 'components_blocks_feature_articles';
+  info: {
+    description: '';
+    displayName: 'Featured Article';
+  };
+  attributes: {
+    excerpt: Schema.Attribute.RichText;
+    headline: Schema.Attribute.String;
+    image: Schema.Attribute.Media<'images'>;
+    link: Schema.Attribute.Component<'elements.link', false>;
+  };
+}
+
+export interface BlocksHeading extends Struct.ComponentSchema {
+  collectionName: 'components_blocks_headings';
+  info: {
+    displayName: 'Heading';
+  };
+  attributes: {
+    heading: Schema.Attribute.String;
+    linkId: Schema.Attribute.String;
+  };
+}
+
+export interface BlocksHeroSection extends Struct.ComponentSchema {
+  collectionName: 'components_blocks_hero_sections';
+  info: {
+    displayName: 'Hero Section';
+  };
+  attributes: {
+    cta: Schema.Attribute.Component<'elements.link', false>;
+    heading: Schema.Attribute.String;
+    image: Schema.Attribute.Media<'images'>;
+    logo: Schema.Attribute.Component<'elements.logo', false>;
+    theme: Schema.Attribute.Enumeration<['turquoise', 'orange']>;
+  };
+}
+
+export interface BlocksInfoBlock extends Struct.ComponentSchema {
+  collectionName: 'components_blocks_info_blocks';
+  info: {
+    displayName: 'Info Block';
+  };
+  attributes: {
+    content: Schema.Attribute.RichText;
+    cta: Schema.Attribute.Component<'elements.link', false>;
+    headline: Schema.Attribute.String;
+    image: Schema.Attribute.Media<'images'>;
+    reversed: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
+    theme: Schema.Attribute.Enumeration<['turquoise', 'orange']>;
+  };
+}
+
+export interface BlocksParagraph extends Struct.ComponentSchema {
+  collectionName: 'components_blocks_paragraphs';
+  info: {
+    displayName: 'Paragraph';
+  };
+  attributes: {
+    content: Schema.Attribute.RichText;
+  };
+}
+
+export interface BlocksSubscribe extends Struct.ComponentSchema {
+  collectionName: 'components_blocks_subscribes';
+  info: {
+    displayName: 'Subscribe';
+  };
+  attributes: {
+    buttonText: Schema.Attribute.String;
+    content: Schema.Attribute.Text;
+    headline: Schema.Attribute.String;
+    placeholder: Schema.Attribute.String;
+  };
+}
+
+export interface ElementsLink extends Struct.ComponentSchema {
+  collectionName: 'components_elements_links';
+  info: {
+    displayName: 'Link';
+  };
+  attributes: {
+    href: Schema.Attribute.String;
+    isExternal: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
+    text: Schema.Attribute.String;
+  };
+}
+
+export interface ElementsLogo extends Struct.ComponentSchema {
+  collectionName: 'components_elements_logos';
+  info: {
+    displayName: 'Logo';
+  };
+  attributes: {
+    image: Schema.Attribute.Media<'images'>;
+    logoText: Schema.Attribute.String;
+  };
+}
+
+export interface LayoutFooter extends Struct.ComponentSchema {
+  collectionName: 'components_layout_footers';
+  info: {
+    displayName: 'Footer';
+  };
+  attributes: {
+    copy: Schema.Attribute.String;
+    logo: Schema.Attribute.Component<'elements.logo', false>;
+    navigation: Schema.Attribute.Component<'elements.link', true>;
+    policies: Schema.Attribute.Component<'elements.link', true>;
+  };
+}
+
+export interface LayoutHeader extends Struct.ComponentSchema {
+  collectionName: 'components_layout_headers';
+  info: {
+    displayName: 'header';
+  };
+  attributes: {
+    cta: Schema.Attribute.Component<'elements.link', false>;
+    logo: Schema.Attribute.Component<'elements.logo', false>;
+    navigation: Schema.Attribute.Component<'elements.link', true>;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'blocks.feature-article': BlocksFeatureArticle;
+      'blocks.heading': BlocksHeading;
+      'blocks.hero-section': BlocksHeroSection;
+      'blocks.info-block': BlocksInfoBlock;
+      'blocks.paragraph': BlocksParagraph;
+      'blocks.subscribe': BlocksSubscribe;
+      'elements.link': ElementsLink;
+      'elements.logo': ElementsLogo;
+      'layout.footer': LayoutFooter;
+      'layout.header': LayoutHeader;
+    }
+  }
+}
